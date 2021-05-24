13f14ed62242b41ecd1000415f57fdf6d053ff3e
JuanitoRojas <juan.rojasbu@gmail.com>
2021-05-24 00:16:55 -0500

    Juan Carlos Rojas
    Proyecto Laravel cargado con las tablas

---
+++ larapp/database/migrations/2021_05_24_051315_create_pueblos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePueblosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pueblos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pueblos');
+    }
+}
